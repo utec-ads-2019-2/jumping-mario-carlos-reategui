806fd3e7ebfed6b0d689f5e08f0a6dd666687bef
carlos-reategui <carlos.reategui@utec.edu.pe>
2019-08-24 13:29:20 -0500

    accepted

--- main.cpp
+++ main.cpp
@@ -1,8 +1,18 @@
 #include <iostream>
 
-using namespace std;
-
 int main() {
-	return EXIT_SUCCESS;
+    int testCases;
+    scanf("%u", &testCases);
+    for(unsigned int i = 0; i < testCases; i++) {
+        unsigned int highJumps = 0, lowJumps = 0, numberOfJumps, currentElevation, nextJump;
+        scanf("%u %u", &numberOfJumps, &currentElevation);
+        for(unsigned int j = 0; j < numberOfJumps - 1; j++) {
+            scanf("%u", &nextJump);
+            if(currentElevation < nextJump) highJumps++;
+            if(currentElevation > nextJump) lowJumps++;
+            currentElevation = nextJump;
+        }
+        printf("Case %u: %u %u\n", i + 1, highJumps, lowJumps);
+    }
+    return 0;
 }
-
